7649f9076c63ce588a5cefbc656f216e0ea417c6
zainab-usman <27100409@lums.edu.pk>
2026-03-19 14:31:09 +0500

    Add green test for hasCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -13,6 +13,9 @@ public class CustomList {
     public void addCity(City city) {
         cities.add(city);
     }
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 
     
 
